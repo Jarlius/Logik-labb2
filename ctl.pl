8bed1b7d090594c8c73c03d89e9f6d7661b6609d
lortisen <jarl.silven@gmail.com>
2016-12-12 15:30:29 +0100

    :hammer: Hjälpmetoder in_list, find_state, koll om label finns i state

--- ctl.pl
+++ ctl.pl
@@ -2,26 +2,25 @@
 % :- use_module(library(lists)).
 
 % Load model, initial state and formula from file.
+% To execute: consult(’your_file.pl’). verify(’input.txt’).
 verify(Input) :-
 see(Input), read(T), read(L), read(S), read(F), seen,
 check(T, L, S, [], F).
 
-% check(T, L, S, U, F)
+% check(T, L, S, U, F).
+% Should evaluate to true iff the sequent below is valid.
+% (T,L), S |-u F
 % T - The transitions in form of adjacency lists
 % L - The labeling
 % S - Current state
 % U - Currently recorded states
 % F - CTL Formula to check.
-%
-% Should evaluate to true iff the sequent below is valid.
-%
-% (T,L), S |-   F
-%             U
-
-% To execute: consult(’your_file.pl’). verify(’input.txt’).
 
 % Literals
-%check(_, L, S, [], X)		:- ...
+check(_, L, S, [], X) :- 
+	find_state(S,L,R),
+	in_list(X,R).
+
 %check(_, L, S, [], neg(X)) :- ...
 
 % And
@@ -33,3 +32,17 @@ check(T, L, S, [], F).
 % EG
 % EF
 % AF
+
+% Helpers
+
+% find_state(S,L,R)
+% Find state S in list L, if it exists, and unify R with S's property
+find_state(_,[],_) :- fail.
+find_state(S,[[S,R]|_],R) :- !.
+find_state(S,[_|T],R) :- find_state(S,T,R).
+
+% in_list(X,L)
+% Verify that element X is present in list L
+in_list(_,[]) :- fail.
+in_list(X,[X|_]) :- !.
+in_list(X,[_|T]) :- in_list(X,T).
