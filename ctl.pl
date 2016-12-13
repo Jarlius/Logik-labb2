d8df760ae72524e91b772501fd0d05d537f75f01
lortisen <jarl.silven@gmail.com>
2016-12-13 08:25:12 +0100

    :hammer: Regel för AllGlobal, med hjälpfunktion

--- ctl.pl
+++ ctl.pl
@@ -17,7 +17,7 @@ check(A, L, S, [], F).
 % F - CTL Formula to check.
 
 % Literals
-check(_, L, S, [], X) :- 
+check(_,L,S,_,X) :- 
 	find_state(S,L,P),
 	in_list(X,P),!.
 % Neg
@@ -26,24 +26,36 @@ check(_, L, S, [], X) :-
 %check(A, L, S, [], and(F,G)) :- ...
 % Or
 % AX
-check(A,L,S,[],ax(X)) :-
+check(A,L,S,_,ax(X)) :-
 	find_state(S,A,P),
-	allnext(A,L,P,X),!.
+	all_next(A,L,P,X),!.
 % EX
-check(A,L,S,[],ex(X)) :-
+check(A,L,S,_,ex(X)) :-
 	find_state(S,A,P),
 	exist_next(A,L,P,X),!.
 % AG
+% borde lägga till tom labellista här, som ska ge true samma som redan nådd
+check(_,_,S,U,ag(_)) :- 
+	in_list(S,U),!.
+check(A,L,S,U,ag(X)) :-
+	check(A,L,S,[],X),
+	find_state(S,A,P),
+	all_global(A,L,P,[S|U],X),!.
 % EG
 % EF
 % AF
 
 % Helpers
 
+% all_global(A,L,P,U,X)
+% Verify that all branches from list P satisfies X.
+all_global(_,_,[],_,_).
+all_global(A,L,[H|T],U,X) :- check(A,L,H,U,ag(X)),all_global(A,L,T,U,X).
+
 % all_next(A,L,P,X)
 % Verify that all states in P satisfies X.
-allnext(_,_,[],_).
-allnext(A,L,[H|T],X) :- check(A,L,H,[],X),allnext(A,L,T,X).
+all_next(_,_,[],_).
+all_next(A,L,[H|T],X) :- check(A,L,H,[],X),all_next(A,L,T,X).
 
 % exist_next(A,L,
 % Verify that at least one state in P satisfies X.
@@ -52,13 +64,13 @@ exist_next(A,L,[H|_],X) :- check(A,L,H,[],X),!.
 exist_next(A,L,[_|T],X) :- exist_next(A,L,T,X).
 
 % find_state(S,L,R)
-% Find state S in list L, if it exists, and unify P with S's Property
+% Find state S in list L, if it exists, and unify P with S's Property.
 find_state(_,[],_) :- fail.
 find_state(S,[[S,P]|_],P) :- !.
 find_state(S,[_|T],P) :- find_state(S,T,P).
 
 % in_list(X,L)
-% Verify that element X is present in list L
+% Verify that element X is present in list L.
 in_list(_,[]) :- fail.
 in_list(X,[X|_]) :- !.
 in_list(X,[_|T]) :- in_list(X,T).
